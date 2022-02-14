a10a3f549ef792ce9a93276cd23d3b56bc30d7ba
Thais Siqueira <sgsilva.thais@gmail.com>
2022-02-14 17:16:08 -0300

    Precisando de implementacoes e ajustes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,117 @@
+let chat = [];
+let username;
+let receiver = "Todos";
+let text;
+let typeMessage = "message";
 
 function SendName(){
     const input = document.querySelector("input");
-    const name = input.value;
+    username = input.value;
+    const section = document.querySelector("section");
+    section.classList.add("hidden");
+    const img = document.querySelector(".loading");
+    img.classList.remove("hidden");
 
-    const request = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', name);
-
-    if (request.then == 400){
-        SolveError();
-    }
-    else {
-        
-    }
+    const resp = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {name: username});
+    resp.then(SolveSuccess);
+    resp.catch(SolveError);  
 }
 
-function SolveError(){
-    alert("Nome já está em uso, tente novamente!");
+function SolveError(resposta){
+    const statusCode = resposta.response.status;
+    if(statusCode == 400)
+    {
+        alert("Nome já está em uso, tente novamente!");
+        const section = document.querySelector("section");
+        section.classList.remove("hidden");
+        const img = document.querySelector(".loading");
+    img.classList.add("hidden");
+        SendName();
+    }
+    console.log(resposta.data);
 }
+
+function SolveSuccess(resposta){
+    const statusCode = resposta.status;
+    if(statusCode == 200) {
+        RenderChat();
+    }
+    console.log(resposta.data);
+}
+
+function RenderChat(){
+    const loading = document.querySelector(".loading");
+    loading.classList.add("hidden");
+    const footer = document.querySelector("footer");
+    footer.classList.remove("hidden"); 
+    const nav = document.querySelector("nav");
+    nav.classList.remove("hidden");
+
+    
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+    promise.then(processingResponse);
+    promise.catch(processingError);
+
+    const main = document.querySelector("main");
+    main.scrollIntoView({block: "end"});
+
+    const ul = document.querySelector("ul");
+    ul.innerHTML = "";
+    for (let i = 0; i < chat.length; i++) {
+        if(chat[i].type == 'message'){
+        ul.innerHTML += `
+            <li class="general-message">
+                <span class="time">${chat[i].time}</span> 
+                <span class="name">${chat[i].from}</span>
+                para
+                <span class="name">${chat[i].to}</span>:
+                <span class="message">${chat[i].text}</span>
+            </li>   
+        `;
+        } else if(chat[i].type == 'status'){
+            ul.innerHTML += `
+            <li class="status-message">
+                <span class="time">${chat[i].time}</span> 
+                <span class="name">${chat[i].from}</span>
+                <span class="message">${chat[i].text}</span>
+            </li>   
+        `;
+        }    else if(chat[i].type == 'private-message'){
+            ul.innerHTML += `
+                <li class="private-message">
+                    <span class="time">${chat[i].time}</span> 
+                    <span class="name">${chat[i].from}</span>
+                    reservadamente para
+                    <span class="name">${chat[i].to}</span>:
+                    <span class="message">${chat[i].text}</span>
+                </li>   
+            `;
+        }
+    }
+
+}
+
+function processingResponse(response){
+    console.log(response);
+    chat = response.data;
+    RenderChat();
+}
+
+function processingError(error){
+    console.log(error.data);
+}
+
+function SendMessage(){
+    const inputMessage = document.querySelector(".input-message");
+    text = inputMessage.value;
+
+    const response = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", {from: username,
+                                                                                       to: receiver,
+                                                                                       text: text,
+                                                                                       type: typeMessage});
+
+    response.then(SolveSuccess);
+    response.catch(FailedToSend);
+
+}
+setInterval(1000, RenderChat);
